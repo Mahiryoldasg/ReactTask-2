037f2126069a94df21d83d655bb39131a34ae8bf
Mahiryoldasg <mahiryoldasg@outlook.com>
2021-06-08 14:28:24 +0300

    Initial commit

--- src/components/Coin/Coin.js
+++ src/components/Coin/Coin.js
@@ -1,19 +1,21 @@
-import React from "react";
-import "./Coin.css";
-import CoinTura from "../../Assets/1-lira-tura.png";
-import CoinYazi from "../../Assets/1-lira-yazi.png";
+import React from 'react';
+import './Coin.css';
+import CoinTura from '../../Assets/1-lira-tura.png';
+import CoinYazi from '../../Assets/1-lira-yazi.png';
 
 const Coin = (props) => {
   return (
-    <div className="Coin-container">
-      <div className={`Coin ${props.flipping ? "Coin-rotate" : ""}`}>
-        <img
-          src={CoinTura}
-          className={props.side === "yazi" ? "Coin-back" : "Coin-front"}
-        />
+    <div className='Coin-container'>
+      <div className={`Coin ${props.flipping ? 'Coin-rotate' : ''}`}>
         <img
           src={CoinYazi}
-          className={props.side === "yazi" ? "Coin-front" : "Coin-back"}
+          alt='Coin'
+          className={props.side === 'yazi' ? 'Coin-front' : 'Coin-back'}
+        />
+        <img
+          src={CoinTura}
+          alt='Coin'
+          className={props.side === 'yazi' ? 'Coin-back' : 'Coin-front'}
         />
       </div>
     </div>
--- src/components/CoinFlipper/CoinFlipper.js
+++ src/components/CoinFlipper/CoinFlipper.js
@@ -1,6 +1,6 @@
-import React, { Component } from "react";
-import Coin from "../Coin/Coin";
-import "./CoinFlipper.css";
+import React, { Component } from 'react';
+import Coin from '../Coin/Coin';
+import './CoinFlipper.css';
 
 class CoinFlipper extends Component {
   constructor(props) {
@@ -8,30 +8,48 @@ class CoinFlipper extends Component {
     // State üzerinde paranın başlangıçtaki durumunu veriyoruz, başlangıçta "tura" olsun.
     // Daha sonra şu anda paranın dönüp dönmeme durumunu da veriyoruz, başlangıçta para atılmamış olduğundan "false" olarak verdik.
     this.state = {
-      side: "tura",
+      side: 'tura',
       flipping: false,
+      count: 0,
+      yazi: 0,
+      tura: 0,
     };
   }
   handleClick = () => {
+    let odds = Math.round(Math.random() * 10);
+    if (odds <= 5) {
+      this.setState((prevState) => ({
+        side: 'yazi',
+        yazi: prevState.yazi + 1,
+      }));
+    } else {
+      this.setState((prevState) => ({
+        side: 'tura',
+        tura: prevState.tura + 1,
+      }));
+    }
     // "At!" butonuna tıkladığımızda paranın dönmesini istiyoruz, bu yüzden "flipping" durumunu "true" yapıyoruz.
-    this.setState({ flipping: true });
+    this.setState((prevState) => ({
+      flipping: true,
+      count: prevState.count + 1,
+    }));
     // 1 saniye kadar dönmesi yeterli, bu yüzden 1 saniye sonra "flipping" durmunu tekrar "false" yapıyoruz.
-    setTimeout(() => this.setState({ flipping: false }), 1000);
+    setTimeout(() => this.setState({ flipping: false }), 800);
   };
 
   render() {
     return (
-      <div className="CoinFlipper">
+      <div className='CoinFlipper'>
         <h1>Yazı mı Tura mı?</h1>
         <Coin side={this.state.side} flipping={this.state.flipping} />
         <button onClick={this.handleClick}>At!</button>
         <p>
           Toplam
-          <strong> 5 </strong>
+          <strong> {this.state.count} </strong>
           atıştan
-          <strong> 3 </strong>ü tura
-          <strong> 2 </strong>
-          si yazı geldi.
+          <strong> {this.state.tura} </strong> tura
+          <strong> {this.state.yazi} </strong>
+          yazı geldi.
         </p>
       </div>
     );
